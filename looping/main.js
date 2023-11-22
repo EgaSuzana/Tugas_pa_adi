9b13dd026501745b3e2dd5654bc2c71624ca2176
EgaSuzana <142204907+EgaSuzana@users.noreply.github.com>
2023-11-22 12:57:27 +0700

    Add files via upload

---
+++ looping/main.js
@@ -0,0 +1,16 @@
+//looping for
+for (let i = 1; i <= 5; i++) {
+    console.log(`${i}. saya pasti bisa gaskeunn`);  
+}
+//looping while
+let a = 1;
+while (a <= 5) {
+    console.log(`${a}. saya pasti bisa gaskeun`);
+    a++;
+}
+//looping do while
+let x = 1;
+do {
+    console.log(`${x}. saya pasti bisa gaskeun`);
+    x++;
+} while (x <= 5);
